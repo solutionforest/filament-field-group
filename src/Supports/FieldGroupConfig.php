97a4d54f4258d724747ee6146a490f5b17bf5f0e
cklei-carly <cklei-carly@users.noreply.github.com>
2024-08-16 05:33:32 +0000

    Fix styling

--- src/Supports/FieldGroupConfig.php
+++ src/Supports/FieldGroupConfig.php
@@ -15,6 +15,7 @@ class FieldGroupConfig
     public static function getFieldGroupModelClass(): string
     {
         $class = config('filament-field-group.models.field_group', FieldGroup::class);
+
         return self::ensureClassExists($class, 'FieldGroup model');
     }
 
@@ -26,20 +27,22 @@ class FieldGroupConfig
     public static function getFieldModelClass(): string
     {
         $class = config('filament-field-group.models.fields', Field::class);
+
         return self::ensureClassExists($class, 'Field model');
     }
 
     /**
      * Ensure that a class exists, or throw an exception.
      *
-     * @param string $class The fully qualified class name
-     * @param string $type A description of the class type (e.g., 'model', 'service')
+     * @param  string  $class  The fully qualified class name
+     * @param  string  $type  A description of the class type (e.g., 'model', 'service')
      * @return string The class name if it exists
+     *
      * @throws \Exception If the class does not exist
      */
     protected static function ensureClassExists(string $class, string $type): string
     {
-        if (!class_exists($class)) {
+        if (! class_exists($class)) {
             throw new \Exception("The {$type} class '{$class}' does not exist. Please check your configuration.");
         }
 
