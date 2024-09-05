babd4ef425d39a32f387f2ddc60e4cc880b288be
carly <carly.cheung@solutionforest.net>
2024-09-05 10:37:00 +0800

    add back missing methods to Facades/FilamentFieldGroup

--- src/Facades/FilamentFieldGroup.php
+++ src/Facades/FilamentFieldGroup.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Facade;
 use SolutionForest\FilamentFieldGroup\FieldTypes\Configs\Contracts\FieldTypeConfig;
 
 /**
+ * @method static void registerModels()
+ * @method static string getFieldGroupModelClass()
+ * @method static void setFieldGroupModelClass(string $modelClass)
+ * @method static string getFieldModelClass()
+ * @method static void setFieldModelClass(string $modelClass)
  * @method static array getFieldTypeOptions()
  * @method static array getFieldTypeGroupedKeyValueOptions()
  * @method static ?string getFieldTypeDisplayValue(string $name)
