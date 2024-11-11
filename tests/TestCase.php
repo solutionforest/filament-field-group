01cc11a7f5509ffa1f2a7526785d562fd776bcf5
carly <carly.cheung@solutionforest.net>
2024-11-11 18:38:21 +0800

    config test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -52,9 +52,7 @@ class TestCase extends Orchestra
     {
         config()->set('database.default', 'testing');
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_filament-advanced-fields_table.php.stub';
+        $migration = include __DIR__.'/../database/migrations/create_advanced_fields_table.php.stub';
         $migration->up();
-        */
     }
 }
