90f0447b38971a898a4197a192231a92032e92a6
carly <carly.cheung@solutionforest.net>
2024-12-02 18:57:14 +0800

    update phpDoc for the model contract for Field/FieldGroup

--- src/Models/Contracts/Field.php
+++ src/Models/Contracts/Field.php
@@ -5,6 +5,19 @@ namespace SolutionForest\FilamentFieldGroup\Models\Contracts;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Spatie\EloquentSortable\Sortable;
 
+/**
+ * @property string $name
+ * @property string $label
+ * @property string $type
+ * @property int $group_id
+ * @property int $sort
+ * @property ?string $instructions
+ * @property bool $mandatory
+ * @property ?string $state_path
+ * @property ?array $config
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
 interface Field extends Sortable
 {
     public function group(): BelongsTo;
--- src/Models/Contracts/FieldGroup.php
+++ src/Models/Contracts/FieldGroup.php
@@ -5,6 +5,14 @@ namespace SolutionForest\FilamentFieldGroup\Models\Contracts;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\EloquentSortable\Sortable;
 
+/**
+ * @property string $title
+ * @property string $name
+ * @property bool $active
+ * @property int $sort
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
 interface FieldGroup extends Sortable
 {
     public function fields(): HasMany;
